5fed6b29eb59d492d55c930bd5e87fb617224c9d
FAIZULLAH <faizullah9181@gmail.com>
2022-03-30 23:00:09 +0530

    Time: 4 ms (77.73%), Space: 9.5 MB (52.00%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,32 +1,51 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+        
+// 		// if matrix have 0 rows or 0 colums
+//         if(n == 0 || m == 0)
+//             return false;
+        
+// 		// treating matrix as array just taking care of endices
+// 		// [0..n*m]
+//         int start = 0, end = m*n - 1;
+        
+//         while(start <= end)
+//         {
+//             int mid = start + (end - start) / 2;
+// 			// a[x] : matrix[x / m][x % m] formulae
+//             int ind = matrix[mid/m][mid%m];
+//             if (target == ind)
+//                 return true;
+// 			// left half
+//             else if(target < ind)
+//                 end = mid - 1;
+//             else
+// 			// right half
+//                 start = mid + 1;       
+//         }
+//         return false;
+//     }
+	
+// };
+
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-		// if matrix have 0 rows or 0 colums
-        if(n == 0 || m == 0)
-            return false;
-        
-		// treating matrix as array just taking care of endices
-		// [0..n*m]
-        int start = 0, end = m*n - 1;
-        
-        while(start <= end)
-        {
-            int mid = start + (end - start) / 2;
-			// a[x] : matrix[x / m][x % m] formulae
-            int ind = matrix[mid/m][mid%m];
-            if (target == ind)
+        int n=matrix.size(),m=matrix[0].size();
+        int r=0,c=m-1;
+        while(r<n && c>=0){
+            if(matrix[r][c]==target){
                 return true;
-			// left half
-            else if(target < ind)
-                end = mid - 1;
+            }
+            if(matrix[r][c]>target)
+                c--;
             else
-			// right half
-                start = mid + 1;       
+                r++;
         }
         return false;
     }
-	
 };
