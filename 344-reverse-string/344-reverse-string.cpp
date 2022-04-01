9a05375ffe60b3d99ee0fa504f43cb8d9568fc85
FAIZULLAH <faizullah9181@gmail.com>
2022-04-01 23:27:19 +0530

    Time: 19 ms (91.83%), Space: 23.2 MB (76.65%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
+//     void reverseString(vector<char>& s) {
         
        
             
-            reverse(s.begin(), s.end());
+             // reverse(s.begin(), s.end());
+        
+        
             
           
        
         
-    }
+//     }
+    
+    void rec(vector<char>& s, int low, int high)
+{
+    if(low >= high)                  // Base Condition
+        return;
+    swap(s[low++], s[high--]);       // makes the problem smaller of same type            
+    rec(s, low, high);
+}
+void reverseString(vector<char>& s) {
+    rec(s, 0, s.size()-1);
+}
 };
