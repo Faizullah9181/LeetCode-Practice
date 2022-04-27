c6659accffe7a74efaebf007bf0f75639985bf9c
FAIZULLAH <faizullah9181@gmail.com>
2022-04-28 00:55:45 +0530

    Time: 4 ms (53.98%), Space: 6 MB (26.71%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long int ans = -1;
+        int s = 0;
+        int e = x;
+        long long int m = s + (e-s)/2;
+        while(s<=e){
+            if(m*m == x){
+                return m;
+            }else if(m*m < x){
+                ans = m;
+                s = m+1;
+            }else if(m*m > x){
+                e = m-1;
+            }
+            
+            m = s + (e-s)/2;
+            
+        }
+        return ans;
+    }
+};
