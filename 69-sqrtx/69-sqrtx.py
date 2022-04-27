7d8c90178fd794827e9a448ebe587e523d19af3d
FAIZULLAH <faizullah9181@gmail.com>
2022-04-28 00:17:11 +0530

    Time: 27 ms (74.31%), Space: 13.2 MB (86.47%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        
+            
+        y=sqrt(x)
+        
+        return int(y)
+        
