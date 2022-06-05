9b353a12b62e7da28f473e7b822b48729f84a475
FAIZULLAH <faizullah9181@gmail.com>
2022-06-05 14:20:12 +0530

    Time: 107 ms (7.36%), Space: 49.7 MB (5.40%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+      bool canWeFillQueen(int row, int col, vector<string> board, int n){
+        int rowBackup = row, colBackup = col;
+        
+        //  diagonal on upper side
+        
+        
+        while(row >=0 && col >= 0){
+            if(board[row--][col--] == 'Q') return false;
+        }
+        
+        //  left side
+        
+        row = rowBackup;
+        col = colBackup;
+        while(row >=0 && col >= 0){
+            if(board[row][col--] == 'Q') return false;
+        }
+        
+        // diagonal on lower side
+        
+        row = rowBackup;
+        col = colBackup;
+        
+        while(row < n && col >= 0){
+            if(board[row++][col--] == 'Q') return false;
+        }        
+        
+        return true;
+    }
+    
+    void fill(vector<string> board, vector<vector<string>> &res, int n, int col){
+        if(col == n){
+            res.push_back(board);
+            return;
+        }
+        
+        for(int row = 0; row < n; row++){
+            if(canWeFillQueen(row, col, board, n)){
+                
+                
+                board[row][col] = 'Q';
+                
+                fill(board, res, n, col+1);
+                
+                board[row][col] = '.'; // backtracking
+            }
+        }
+    }
+    
+    int totalNQueens(int n) {
+          vector<vector<string>> res;
+        vector<string> board(n);
+        string str(n, '.');
+        for(int i = 0; i < n; i++){
+            board[i] = str;
+        }
+        
+        fill(board, res, n, 0);
+        
+        return res.size();
+    }
+};
