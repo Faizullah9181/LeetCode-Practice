60eccb22a92dcf8756d04409827032486e5b0cc4
FAIZULLAH <faizullah9181@gmail.com>
2022-06-22 14:47:35 +0530

    Time: 20 ms (21.13%), Space: 10.1 MB (37.94%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-    
-    int partition(vector<int>& nums, int s, int e) {
-        int pivot = nums[e];
-        int i =s;
-        for(int j=s; j<=e-1; j++) {
-            if(nums[j] > pivot) {
-                swap(nums[i], nums[j]);
-                i++;
+    int findKthLargest(vector<int>& nums, int k) {
+             priority_queue<int,vector<int>,greater<int>> pq;
+
+        
+        int n=nums.size();
+          for (int i = 0; i < k; i++) {
+        pq.push( nums[i]);
+              
+              
+              
+    }
+        
+        for(int i=k;i<n;i++){
+            
+            if(nums[i]>pq.top()){
+                
+                
+                pq.pop();
+                pq.push(nums[i]);
             }
         }
         
-        swap(nums[i], nums[e]);
-        return i;
+        
+        
+        int ans=pq.top();
+        
+        return ans;
+
     }
-    
-     int quickSelect(vector<int>& nums, int k, int s, int e) {
-        int id = partition(nums, s, e);
-        if(id == k-1) return nums[k-1];
-        else if(id < k-1) {
-            // search right part
-            return quickSelect(nums, k, id+1, e);
-        } else {
-            //search left part
-            return quickSelect(nums, k, s, id-1);
-        }
-    }
-    
-    int findKthLargest(vector<int>& nums, int k) {
-        if(k>nums.size()) {
-            // if k is larger than nums.size, this is not a valid scenario
-            return -1;
-        } else {
-            int s = 0;
-            int e =nums.size()-1;
-            return quickSelect(nums, k, s, e);
-        }
-    }
-    
-    
-    
-   
 };
