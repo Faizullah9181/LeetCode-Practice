9e15c3fa3b1f44d5fa43d5b9203c6836422ee8c8
FAIZULLAH <faizullah9181@gmail.com>
2022-06-25 21:14:23 +0530

    Time: 47 ms (61.31%), Space: 14.5 MB (30.84%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -10,13 +10,7 @@ public:
 			arr.push_back(matrix[i][j]); 
 		} 
 	} 
-               
-//         for(int i=0;i<arr.size();i++){
-        
-//         if(arr[i]==arr[i+1]) return arr[k];
-	
-//         }
-        
+
         sort(arr.begin(),arr.end());
         
     return arr[k-1];
