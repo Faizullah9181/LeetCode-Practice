19d280215ef30a369dfbc628fac6df93af93d12b
FAIZULLAH <faizullah9181@gmail.com>
2022-06-26 19:55:12 +0530

    Time: 4 ms (76.75%), Space: 9.6 MB (5.22%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,20 +1,34 @@
-
-
-
+// Binary serach
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size(),m=matrix[0].size();
-        int r=0,c=m-1;
-        while(r<n && c>=0){
-            if(matrix[r][c]==target){
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+		// if matrix have 0 rows or 0 colums
+        if(n == 0 || m == 0)
+            return false;
+        
+		// treating matrix as array just taking care of endices
+		// [0..n*m]
+        int start = 0, end = m*n - 1;
+        
+        while(start <= end)
+        {
+            int mid = start + (end - start) / 2;
+			// a[x] : matrix[x / m][x % m] formulae
+            int ind = matrix[mid/m][mid%m];
+            if (target == ind)
                 return true;
-            }
-            if(matrix[r][c]>target)
-                c--;
+			// left half
+            else if(target < ind)
+                end = mid - 1;
             else
-                r++;
+			// right half
+                start = mid + 1;       
         }
         return false;
     }
+	
 };
+
