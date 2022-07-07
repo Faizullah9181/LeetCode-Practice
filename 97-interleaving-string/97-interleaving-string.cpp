522c5e9cb2cba7827422c794dacb1b8b5f38e2a4
FAIZULLAH <faizullah9181@gmail.com>
2022-07-08 00:35:31 +0530

    Time: 51 ms (24.03%), Space: 34.3 MB (15.46%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -2,8 +2,8 @@ class Solution
 {
     public:
         vector<vector < int>> memo;
-    string s1, s2, s3;
-    int dp(int i, int j)
+
+    int dp(int i, int j, string s1, string s2, string s3)
     {
 
         int k = i + j;
@@ -20,27 +20,25 @@ class Solution
         if (i < s1.size() && s1[i] == s3[k])
         {
 
-            ans |= dp(i + 1, j);
+            ans |= dp(i + 1, j,s1,s2,s3);
         }
 
         if (j < s2.size() && s2[j] == s3[k])
         {
 
-            ans |= dp(i, j + 1);
+            ans |= dp(i, j + 1,s1,s2,s3);
         }
 
         return memo[i][j] = ans;
     }
     bool isInterleave(string s1, string s2, string s3)
     {
-        this->s1 = s1;
-        this->s2 = s2;
-        this->s3 = s3;
+      
 
         if (s1.size() + s2.size() != s3.size()) return false;
 
         memo.resize(s1.size() + 1, vector<int> (s2.size() + 1, -1));
 
-        return dp(0, 0);
+        return dp(0, 0,s1,s2,s3);
     }
 };
