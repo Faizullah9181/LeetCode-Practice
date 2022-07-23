db942a12c0c0e9b68b436351ea6993296d1dc63d
FAIZULLAH <faizullah9181@gmail.com>
2022-07-24 00:53:33 +0530

    Time: 39 ms (60.26%), Space: 13.6 MB (67.75%) - LeetHub

---
+++ 415-add-strings/415-add-strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        
+        l=int(num1)+int(num2)
+        
+        return str(l)
