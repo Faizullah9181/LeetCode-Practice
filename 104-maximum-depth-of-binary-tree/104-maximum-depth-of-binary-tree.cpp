a2085976f50f35dfbe56d1519623b37270a28a10
FAIZULLAH <faizullah9181@gmail.com>
2022-07-24 16:53:50 +0530

    Time: 9 ms (78.67%), Space: 18.9 MB (55.94%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -1,32 +1,30 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-/*
+class Solution
+{
+    public:
+        int maxDepth(TreeNode *root)
+        {
+            
+            
+            if(root==NULL)
+                
+                return 0;
 
-class Solution {
-public:
-   int maxDepth(TreeNode* root) {
-	if(!root) return 0;
-        int maxLeft = maxDepth(root->left);
-        int maxRight = maxDepth(root->right);
-        return max(maxLeft, maxRight)+1;
-   }
-   };
-   
-   */
-class Solution {
-public:
-   int maxDepth(TreeNode* root) {
-	if (root==NULL) return 0; 
-		return max(maxDepth(root->left)+1, maxDepth(root->right)+1);
-   }
-   };
+            int lefth = maxDepth(root->left);
+            int righth = maxDepth(root->right);
 
+            int ans = max(lefth, righth) + 1;
+
+            return ans;
+        }
+};
