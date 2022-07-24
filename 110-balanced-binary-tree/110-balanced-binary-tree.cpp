2640c13a026a19673e6ef9e42494771a817ec351
FAIZULLAH <faizullah9181@gmail.com>
2022-07-25 00:43:42 +0530

    Time: 8 ms (96.45%), Space: 22.3 MB (5.16%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -1,12 +1,12 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 // class Solution {
@@ -43,41 +43,42 @@
 //     }
 // };
 
-class Solution{
+class Solution
+{
     public:
-    //Function to check whether a binary tree is balanced or not.
-    pair<bool, int> isBalancedFast(TreeNode* root) {
-                // base case
-        if(root == NULL)
+       	//Function to check whether a binary tree is balanced or not.
+        pair<bool, int> isBalancedFast(TreeNode *root)
         {
-            pair<bool, int> p = make_pair(true, 0);
-            return p;
+           	// base case
+            if (root == NULL)
+            {
+                pair<bool, int> p = make_pair(true, 0);
+                return p;
+            }
+
+            pair<int, int> left = isBalancedFast(root->left);
+            pair<int, int> right = isBalancedFast(root->right);
+
+            bool leftAns = left.first;
+            bool rightAns = right.first;
+
+            bool diff = abs(left.second - right.second) <= 1;
+
+            pair<bool, int> ans;
+            ans.second = max(left.second, right.second) + 1;
+
+            if (leftAns && rightAns && diff)
+            {
+                ans.first = true;
+            }
+            else
+            {
+                ans.first = false;
+            }
+            return ans;
         }
-        
-        pair<int,int> left = isBalancedFast(root->left);
-        pair<int,int> right = isBalancedFast(root->right);
-        
-        
-        bool leftAns = left.first;
-        bool rightAns = right.first;
-        
-        bool diff = abs (left.second - right.second ) <=1;
-        
-        pair<bool,int> ans;
-        ans.second = max(left.second, right.second) + 1;
-        
-        if(leftAns && rightAns && diff) {
-           ans.first = true;
-        }
-        else
-        {
-            ans.first = false;
-        }
-        return ans;
-    }
     bool isBalanced(TreeNode *root)
     {
         return isBalancedFast(root).first;
     }
-    
 };
