558535a6be1c3e9c522c60f2b288954fa1d48e77
FAIZULLAH <faizullah9181@gmail.com>
2022-08-03 19:12:11 +0530

    Time: 24 ms (90.66%), Space: 19.2 MB (83.25%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        
+        int size = nums.size();
+        
+        int prevzero =0;
+        
+        for(int i=0;i<size;i++){
+            
+            if(nums[i] != 0){
+                
+                swap(nums[prevzero],nums[i]);
+            
+                    prevzero++;
+            
+            }
+            
+        }
+        
+        
+    }
+};
