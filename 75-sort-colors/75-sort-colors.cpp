faae78a13f6fd44fbf1817ffccee90859e271e32
FAIZULLAH <faizullah9181@gmail.com>
2022-08-03 16:34:23 +0530

    Time: 4 ms (45.02%), Space: 8.3 MB (23.78%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -3,29 +3,32 @@ public:
     void sortColors(vector<int>& nums) {
          // Using dutch national flag algo
         
-        int low=0;
-        int high=nums.size()-1;
-        int mid=0;
+//         int low=0;
+//         int high=nums.size()-1;
+//         int mid=0;
         
-        while(mid<=high){
-            switch(nums[mid]){
+//         while(mid<=high){
+//             switch(nums[mid]){
                     
-                case 0:
-                    swap(nums[low++],nums[mid++]);
+//                 case 0:
+//                     swap(nums[low++],nums[mid++]);
                         
-                        break;
+//                         break;
             
             
-            case 1:
-            mid++ ;
-            break;
+//             case 1:
+//             mid++ ;
+//             break;
             
-            case 2:
-              swap(nums[mid],nums[high--]);
-               break;
+//             case 2:
+//               swap(nums[mid],nums[high--]);
+//                break;
                     
-            }
+//             }
             
-        }
+//         }
+        
+        
+        return sort(nums.begin(),nums.end());
     }
 };
