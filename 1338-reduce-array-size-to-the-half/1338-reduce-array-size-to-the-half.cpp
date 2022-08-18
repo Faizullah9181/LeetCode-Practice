f7d0e8543c98e306970afc541095a2bf71cd02b7
FAIZULLAH <faizullah9181@gmail.com>
2022-08-18 21:01:25 +0530

    Time: 461 ms (22.49%), Space: 80.1 MB (39.23%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+    public:
+        int minSetSize(vector<int> &arr)
+        {
+
+            int n = arr.size();
+            int i;
+            map<int, int> m;
+
+            for (auto it: arr)
+            {
+
+                m[it]++;
+            }
+
+            vector<int> ans;
+
+            for (auto i: m)
+            {
+
+                ans.push_back(i.second);
+            }
+
+            sort(ans.begin(), ans.end(), greater<int> ());
+
+            int cnt = 0;
+
+            int total = n;
+
+            for (auto i: ans)
+            {
+                if (total <= n / 2)
+                {
+                    break;
+                }
+                cnt++;
+                total -= i;
+            }
+            return cnt;
+        }
+};
