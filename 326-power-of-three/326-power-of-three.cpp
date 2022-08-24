233fdeaceaf0f34cc654fa5fbfa82f67c6006711
FAIZULLAH <faizullah9181@gmail.com>
2022-08-24 21:57:00 +0530

    Time: 38 ms (40.21%), Space: 5.9 MB (72.69%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+    public:
+        bool isPowerOfThree(int n)
+        {
+            int k = pow(3,19);
+            
+            
+            return n > 0 && k % n == 0;
+        }
+};
