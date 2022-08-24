c941e66afbc8058f63d9ebe8e80623814dcdf2af
FAIZULLAH <faizullah9181@gmail.com>
2022-08-24 21:44:33 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (71.30%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,7 +1,18 @@
-class Solution {
-public:
-    bool isPowerOfFour(int n) {
-        if(n==0) return false;
-        return ( ceil(log(n)/log(4))==floor(log(n)/log(4)));
-    }
+class Solution
+{
+    public:
+        bool isPowerOfFour(int n)
+        {
+            if (n == 0) return false;
+           	// return (ceil(log(n)/log(4))==floor(log(n)/log(4)));
+
+            while (n % 4 == 0)
+            {
+
+                n /= 4;
+
+               
+            }
+             return n == 1;
+        }
 };
