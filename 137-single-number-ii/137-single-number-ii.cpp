48ef0bd21290394adf3e09c948376b684a26ca86
FAIZULLAH <faizullah9181@gmail.com>
2022-08-27 20:31:09 +0530

    Time: 14 ms (51.93%), Space: 10.1 MB (23.95%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -4,14 +4,35 @@ class Solution
         int singleNumber(vector<int> &nums)
         {
 
-            int ones=0, twos = 0;
+           	//             int ones = 0, twos = 0;
 
-            for (int i = 0; i < nums.size(); i++)
+           	//             for (int i = 0; i < nums.size(); i++)
+           	//             {
+
+           	//                 ones = (ones ^ nums[i]) &(~twos);
+           	//                 twos = (twos ^ nums[i]) &(~ones);
+           	//             }
+           	//             return ones;
+
+            unordered_map<int, int> um;
+            int num;
+
+            for (auto m: nums)
             {
 
-                ones = (ones ^ nums[i]) &(~twos);
-                twos = (twos ^ nums[i]) &(~ones);
+                um[m]++;
             }
-            return ones;
+
+            for (auto m: um)
+            {
+
+                if (m.second == 3) continue;
+
+                else
+
+                    num = m.first;
+            }
+
+            return num;
         }
 };
