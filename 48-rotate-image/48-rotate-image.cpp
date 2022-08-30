1b95fddcf09b91ee080aebe6cfd8fa24e0380f91
FAIZULLAH <faizullah9181@gmail.com>
2022-08-30 18:58:31 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (92.76%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,21 +1,23 @@
-class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
-       
-        
-        reverse(matrix.begin(), matrix.end());
-        int m=matrix.size();
-        int n=matrix[0].size();
-        
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<i;j++){
-                
-          
-                swap(matrix[i][j],matrix[j][i]);
+class Solution
+{
+    public:
+        void rotate(vector<vector < int>> &matrix)
+        {
 
+            reverse(matrix.begin(), matrix.end());
+
+            int m = matrix.size();
+            int n = matrix[0].size();
+
+            for (int i = 0; i < m; i++)
+            {
+
+                for (int j = 0; j < i; j++) {
+                    
+                    
+                    swap(matrix[i][j],matrix[j][i]);
+                    
+                }
             }
         }
-       
-    }
 };
