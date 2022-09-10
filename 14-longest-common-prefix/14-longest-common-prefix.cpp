8f8c66f68b2a6080521ab3525a8192c5d1254ff2
FAIZULLAH <faizullah9181@gmail.com>
2022-09-10 07:32:01 -0700

    Time: 8 ms (46.55%), Space: 9.1 MB (95.36%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+        string longestCommonPrefix(vector<string> &strs)
+        {
+            int n = strs.size();
+            string str;
+            sort(strs.begin(), strs.end());
+            string first = strs[0];
+            cout<<first;
+            string last = strs[n - 1];
+            for (int i = 0; i < first.size(); i++)
+            {
+                if (first[i] == last[i])
+                {
+                    str += first[i];
+                }
+                else
+                    break;
+            }
+            return str;
+        }
+};
