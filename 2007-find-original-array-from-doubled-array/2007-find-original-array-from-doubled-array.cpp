6a6f33960ea3d3cc2f037e7d07ad79f675985433
FAIZULLAH <faizullah9181@gmail.com>
2022-09-15 16:10:30 +0530

    Time: 1383 ms (5.07%), Space: 196.3 MB (10.23%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,48 +1,64 @@
 class Solution
 {
     public:
-        vector<int> findOriginalArray(vector<int> &changed)
-        {
-            int n = changed.size();
+       	//         vector<int> findOriginalArray(vector<int> &changed)
+       	//         {
+       	//             int n = changed.size();
 
-            unordered_map<int, int> ump;
+       	//             unordered_map<int, int> ump;
+
+       	//             vector<int> ans;
+
+       	//             if (n % 2 != 0) return ans;
+
+       	//             for (auto it: changed)
+       	//             {
+       	//                 ump[it]++;
+       	//             }
+       	//             sort(changed.begin(), changed.end());
+
+       	//             for (auto it: changed)
+       	//             {
+       	//                 if (ump[it] == 0) continue;
+
+       	//                 if (ump[2 *it] == 0)
+       	//                 {
+       	//                     return {};
+       	//                 }
+
+       	//                 if (ump[it] >= 1 && ump[2 *it] >= 1)
+       	//                 {
+       	//                     ans.push_back(it);
+       	//                     ump[it]--;
+       	//                     ump[2 *it]--;
+       	//                 }
+       	//             }
+
+       	//             return ans;
+       	//         }
+
+        vector<int> findOriginalArray(vector<int> & changed)
+        {
+
+            if (changed.size()  % 2  != 0)
+                return {};
 
             vector<int> ans;
+            multiset<int> s;
 
-            if (n % 2 != 0) return ans;
+            for (auto x: changed)
+                s.insert(x);
 
-            for (auto it: changed)
+            while (!s.empty())
             {
-                ump[it]++;
-            }
-            sort(changed.begin(), changed.end());
+                int ele = *s.begin();
+                s.erase(s.find(ele));
 
-            for (auto it: changed)
-            {
-                if (ump[it] == 0) continue;
-
-                if (ump[2 *it] == 0)
-                {
+                if (s.find(2 *ele) == s.end())
                     return {};
-                }
-
-                if (ump[it] >= 1 && ump[2 *it] >= 1)
-                {
-                    ans.push_back(it);
-                    ump[it]--;
-                    ump[2 *it]--;
-                }
+                s.erase(s.find(2 *ele));
+                ans.push_back(ele);
             }
-
             return ans;
         }
 };
-
-
-
-
-
-
-
-
-
