11558576727d266288cbc4109eb58a59e15f3fa1
FAIZULLAH <faizullah9181@gmail.com>
2022-09-26 19:10:13 +0530

    Time: 3 ms (64.72%), Space: 8.4 MB (17.71%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -1,11 +1,11 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *   int val;
+ *   ListNode * next;
+ *   ListNode() : val(0), next(nullptr) {}
+ *   ListNode(int x) : val(x), next(nullptr) {}
+ *   ListNode(int x, ListNode *next) : val(x), next(next) {}
  *};
  */
 class Solution
@@ -13,19 +13,31 @@ class Solution
     public:
         int getDecimalValue(ListNode *head)
         {
-            vector<int> v;
+           	//             vector<int> v;
+           	//             while (head != NULL)
+           	//             {
+           	//                 v.push_back(head->val);
+           	//                 head = head->next;
+           	//             }
+
+           	//             int ans = 0;
+           	//             for (int i = 0; i < v.size(); i++)
+           	//             {
+           	//                 ans += v[i] *pow(2, v.size() - i - 1);
+           	//             }
+
+           	//             return ans;
+
+            int res=0;
+
             while (head != NULL)
             {
-                v.push_back(head->val);
+
+                res = res*2 + head->val;
+
                 head = head->next;
             }
 
-            int ans = 0;
-            for (int i = 0; i < v.size(); i++)
-            {
-                ans += v[i] *pow(2, v.size() - i - 1);
-            }
-
-            return ans;
+            return res;
         }
 };
