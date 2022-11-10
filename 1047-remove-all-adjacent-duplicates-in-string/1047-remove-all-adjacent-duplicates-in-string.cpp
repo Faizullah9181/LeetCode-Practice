383934081d8aa82c837921c6bca85fba7c32bb7b
FAIZULLAH <faizullah9181@gmail.com>
2022-11-10 18:03:01 +0530

    Time: 59 ms (50.29%), Space: 11 MB (48.01%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+        string removeDuplicates(string s)
+        {
+
+            string ans = "";
+
+            for (auto &i: s)
+            {
+
+                if (i == ans.back())
+                {
+
+                    ans.pop_back();
+                }
+                else
+                {
+
+                    ans += i;
+                }
+            }
+
+            return ans;
+        }
+};
