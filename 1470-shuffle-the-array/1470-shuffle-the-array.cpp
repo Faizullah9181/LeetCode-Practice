f7b7e49b78c92e7272228f93f3943de41be2263f
FAIZULLAH <faizullah9181@gmail.com>
2023-02-08 11:30:52 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (31.58%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,20 +1,16 @@
-class Solution
-{
-    public:
-        vector<int> shuffle(vector<int> &nums, int n)
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> firstHalf;
+        for(int i=0; i<n; i++)
         {
-
-            vector<int> ans;
-
-            for (int i = 0; i < n; i++)
-            {
-
-                ans.push_back(nums[i]);
-    
-
-                ans.push_back(nums[i + n]);
-            }
-
-            return ans;
+            firstHalf.push_back(nums[i]);
         }
+        for(int i=0, j=n; i<n; i++,j++)
+        {
+            nums[2*i]=firstHalf[i];
+            nums[2*i+1]=nums[j];
+        }
+        return nums;
+    }
 };
